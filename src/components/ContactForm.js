4228217f62623944c8dabacd29e4e8a9d815cc65
Mark Edwards <109709559+mark-227-g@users.noreply.github.com>
2023-07-02 10:47:07 -0400

    finished

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 
-import { checkMessage, validateEmail } from '../utils/helpers';
+import { validateEmail } from '../utils/helpers';
 
 function Form() {
   // Create state variables for the fields in the form
   // We are also setting their initial values to an empty string
   const [email, setEmail] = useState('');
   const [userName, setUserName] = useState('');
-  const [message, setMessege] = useState('');
+  const [inputMessage, setMessege] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
   const handleInputChange = (e) => {
@@ -15,14 +15,13 @@ function Form() {
     const { target } = e;
     const inputType = target.name;
     const inputValue = target.value;
-
     // Based on the input type, we set the state of either email, username, and password
     if (inputType === 'email') {
       setEmail(inputValue);
     } else if (inputType === 'userName') {
       setUserName(inputValue);
-    } else {
-      setMessege(message);
+    } else if (inputType === 'inputMessage'){
+      setMessege(inputValue);
     }
   };
 
@@ -31,19 +30,23 @@ function Form() {
     e.preventDefault();
 
     // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-    if (!validateEmail(email) || !userName) {
-      setErrorMessage('Email or username is invalid');
+    if(!userName){
+      setErrorMessage('Name is required');
+      return;
+    }
+    if (!validateEmail(email)) {
+      setErrorMessage('Email is invalid');
       // We want to exit out of this code block if something is wrong so that the user can correct it
       return;
       // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
     }
-    if (!checkMessage(message)) {
+    if (!inputMessage) {
       setErrorMessage(
-        `Message is required: ${userName}`
+        `Message is required`
       );
       return;
     }
-    alert(`Hello ${userName}`);
+    setErrorMessage("Your message has been sent")
 
     // If everything goes according to plan, we want to clear out the input after a successful registration.
     setUserName('');
@@ -61,7 +64,7 @@ function Form() {
           name="userName"
           onChange={handleInputChange}
           type="text"
-          placeholder="username"
+          placeholder="Name"
         /></div>
         <div>Email address:</div>
         <div><input
@@ -72,10 +75,12 @@ function Form() {
           placeholder="email"
         /></div>
         <div>Message</div>
-        <div><input
-          value={message}
-          name="message"
+        <div><textarea
+          value={inputMessage}
+          name="inputMessage"
           onChange={handleInputChange}
+          rows="5"
+          cols="30"
           type="text"
           placeholder="Message"
         /></div>
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -8,12 +8,6 @@ export default function Contact() {
       <h1>Contact</h1>
       <p>
         <ContactForm />
-      <ul>
-          <li>Phone: 1-973-953-6258</li>
-          <li><a href="mailto:marko65e@gmail.com">email: marko65e@gmail.com</a></li>
-          <li><a target="_blank" href="https://www.linkedin.com/in/mark-charles-edwards/" rel="noreferrer">LinkedIn: https://www.linkedin.com/in/mark-charles-edwards/</a></li>
-          <li><a target="_blank" href="https://github.com/mark-227-g/challenge1" rel="noreferrer"> Github:https://github.com/mark-227-g/challenge1</a></li>
-      </ul>
     </p>
     </div>
   );
